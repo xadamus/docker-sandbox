ff809b078438e78d868b0e016929c2bf7638e809
Adam Wątroba <adam.j.watroba@gmail.com>
2022-06-02 15:56:44 +0200

    Deployment final configuration

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t xadamus/docker-sandbox-client:latest -t xadamus/docker-sandbox-client:$SHA -f ./client/Dockerfile ./client
+docker build -t xadamus/docker-sandbox-server:latest -t xadamus/docker-sandbox-server:$SHA -f ./server/Dockerfile ./server
+docker build -t xadamus/docker-sandbox-worker:latest -t xadamus/docker-sandbox-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push xadamus/docker-sandbox-client:latest
+docker push xadamus/docker-sandbox-server:latest
+docker push xadamus/docker-sandbox-worker:latest
+docker push xadamus/docker-sandbox-client:$SHA
+docker push xadamus/docker-sandbox-server:$SHA
+docker push xadamus/docker-sandbox-worker:$SHA
+
+kubectl apply -f k8s/
+kubectl set image deployments/server-deployment server=xadamus/docker-sandbox-server:$SHA
+kubectl set image deployments/client-deployment server=xadamus/docker-sandbox-client:$SHA
+kubectl set image deployments/worker-deployment server=xadamus/docker-sandbox-worker:$SHA
